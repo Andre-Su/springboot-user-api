d297645a1553fe600e88efd3c912cbd711824872
Andre-Su <aascnpc@gmail.com>
2024-11-23 22:23:34 -0300

    Metodo GET todos Metodo GET com ID Metodo PUT com ID Metodo POST Metodo DELETE

--- src/main/java/com/example/bootcamp/controllers/MunicipioController.java
+++ src/main/java/com/example/bootcamp/controllers/MunicipioController.java
@@ -3,6 +3,7 @@ package com.example.bootcamp.controllers;
 import com.example.bootcamp.dtos.MunicipioDto;
 import com.example.bootcamp.models.MunicipioVo;
 import com.example.bootcamp.services.MunicipioService;
+import com.example.bootcamp.services.UfService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,34 +12,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/municipios")
+@RequestMapping("/municipio")
 public class MunicipioController {
 
     @Autowired
     private MunicipioService municipioService;
+    @Autowired
+    private UfService ufService;
 
     @GetMapping
-    public List<MunicipioVo> getAllMunicipios() {
-        return municipioService.getAllMunicipios();
+    public ResponseEntity<List<MunicipioVo>> getAllMunicipios() {
+        return ResponseEntity.status(HttpStatus.OK).body(municipioService.getAllMunicipios());
     }
 
-    @GetMapping("/?id={id}")
-    public MunicipioVo getMunicipioById(@PathVariable long id) {
-        return municipioService.getMunicipioById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<MunicipioVo> getMunicipioById(@PathVariable long id) {
+        return ResponseEntity.status(HttpStatus.OK).body(municipioService.getMunicipioById(id));
     }
 
     @PostMapping
     public ResponseEntity<MunicipioVo> createMunicipio(@RequestBody MunicipioDto municipioDto) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(municipioService.createMunicipio(municipioDto)) ;
+        if (ufService.existsById(municipioDto.codigoUF())) {
+            return ResponseEntity.status(HttpStatus.CREATED).body(municipioService.createMunicipio(municipioDto));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
-    @PutMapping("/?id={id}")
-    public MunicipioVo updateMunicipio(@PathVariable long id, @RequestBody MunicipioDto municipioDto) {
-        return municipioService.updateMunicipio(id, municipioDto);
+    @PutMapping("/{id}")
+    public ResponseEntity<MunicipioVo> updateMunicipio(@PathVariable long id, @RequestBody MunicipioDto municipioDto) {
+        if (municipioService.existsById(id) && ufService.existsById(municipioDto.codigoUF())) {
+            return ResponseEntity.status(HttpStatus.OK).body(municipioService.updateMunicipio(id, municipioDto));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
     }
 
-    @DeleteMapping("/?id={id}")
-    public void deleteMunicipio(@PathVariable long id) {
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMunicipio(@PathVariable long id) {
         municipioService.deleteMunicipio(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
