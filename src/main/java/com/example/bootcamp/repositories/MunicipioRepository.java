bd5710c213cc572e0bd1ef65e0ad1e53c62a2d66
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2024-11-24 14:03:59 -0300

    Adicionado funções de encontrar por nome e uf > encontrar por uf ainda em construção

--- src/main/java/com/example/bootcamp/repositories/MunicipioRepository.java
+++ src/main/java/com/example/bootcamp/repositories/MunicipioRepository.java
@@ -1,7 +1,12 @@
 package com.example.bootcamp.repositories;
 
 import com.example.bootcamp.models.MunicipioVo;
+import com.example.bootcamp.models.UfVo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MunicipioRepository extends JpaRepository<MunicipioVo, Long> {
+    public Optional<MunicipioVo> findByNome(String nome);
+    public Optional<MunicipioVo> findByUf(UfVo ufVo);
 }
