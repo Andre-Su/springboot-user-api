6759d4cb0ecc15eca53e74281d506aaa96a67fc8
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2024-11-24 12:06:03 -0300

    Métodos encontrar por nome e sigla

--- src/main/java/com/example/bootcamp/repositories/UfRepository.java
+++ src/main/java/com/example/bootcamp/repositories/UfRepository.java
@@ -3,5 +3,9 @@ package com.example.bootcamp.repositories;
 import com.example.bootcamp.models.UfVo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UfRepository extends JpaRepository<UfVo, Long> {
+    Optional<UfVo> findByNome(String nome);
+    Optional<UfVo> findBySigla(String sigla);
 }
