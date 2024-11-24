9a7529aa064108fabd64d5c1f3fe86478abdb237
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2024-11-24 14:46:45 -0300

    find by MunicipioVo

--- src/main/java/com/example/bootcamp/repositories/BairroRepository.java
+++ src/main/java/com/example/bootcamp/repositories/BairroRepository.java
@@ -1,10 +1,13 @@
 package com.example.bootcamp.repositories;
 
 import com.example.bootcamp.models.BairroVo;
+import com.example.bootcamp.models.MunicipioVo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface BairroRepository extends JpaRepository<BairroVo, Long> {
     Optional<BairroVo> findByNome(String nome);
+    List<Optional<BairroVo>> findByMunicipio(MunicipioVo municipioVo);
 }
