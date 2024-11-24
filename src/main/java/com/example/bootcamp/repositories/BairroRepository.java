505f8cee189028cdd2082897f782252c40d1d855
Andre-Su <aascnpc@gmail.com>
2024-11-24 01:09:48 -0300

    Adicionada função de busca por nome

--- src/main/java/com/example/bootcamp/repositories/BairroRepository.java
+++ src/main/java/com/example/bootcamp/repositories/BairroRepository.java
@@ -3,5 +3,8 @@ package com.example.bootcamp.repositories;
 import com.example.bootcamp.models.BairroVo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BairroRepository extends JpaRepository<BairroVo, Long> {
+    Optional<BairroVo> findByNome(String nome);
 }
