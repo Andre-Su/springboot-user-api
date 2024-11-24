5151e61a97e475f2c7c0f3efd52a81b49eb8b3d3
Andre-Su <aascnpc@gmail.com>
2024-11-24 01:10:52 -0300

    Médotos RESTful completos

--- src/main/java/com/example/bootcamp/controllers/BairroController.java
+++ src/main/java/com/example/bootcamp/controllers/BairroController.java
@@ -1,4 +1,59 @@
 package com.example.bootcamp.controllers;
 
+import com.example.bootcamp.dtos.BairroDto;
+import com.example.bootcamp.models.BairroVo;
+import com.example.bootcamp.services.BairroService;
+import com.example.bootcamp.services.MunicipioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/bairro")
 public class BairroController {
+    @Autowired
+    private BairroService bairroService;
+    @Autowired
+    private MunicipioService municipioService;
+
+    @GetMapping
+    public ResponseEntity<List<BairroVo>> getAllBairros() {
+        return ResponseEntity.status(HttpStatus.OK).body(bairroService.findAll());
+    }
+
+    @GetMapping("/id")
+    public ResponseEntity<BairroVo> getBairroById(@RequestParam(value = "id", required = false) long id) {
+        Optional<BairroVo> bairroVoOptional = bairroService.findById(id);
+        return bairroVoOptional.map(bairroVo -> ResponseEntity.status(HttpStatus.OK).body(bairroVo))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
+
+    @GetMapping("/nome")
+    public ResponseEntity<BairroVo> getBairroByNome(@RequestParam(value = "nome", required = false) String nome) {
+        Optional<BairroVo> bairroVoOptional = bairroService.findByNome(nome);
+        return bairroVoOptional.map(bairroVo -> ResponseEntity.status(HttpStatus.OK).body(bairroVo)).orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(null));
+    }
+
+    @PostMapping
+    public ResponseEntity<BairroVo> createBairro(@RequestBody BairroDto bairroDto) {
+        if(municipioService.existsById(bairroDto.codigoMunicipio())) {
+            return ResponseEntity.status(HttpStatus.OK).body(bairroService.save(bairroDto));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<BairroVo> updateBairro(@PathVariable long id, @RequestBody BairroDto bairroDto) {
+        Optional<BairroVo> bairroVoOptional = bairroService.findById(id);
+        if(bairroVoOptional.isPresent() && municipioService.existsById(bairroDto.codigoMunicipio())) {
+            return ResponseEntity.status(HttpStatus.OK).body(bairroService.updateBairro(id, bairroDto));
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
 }
--- src/main/java/com/example/bootcamp/services/BairroService.java
+++ src/main/java/com/example/bootcamp/services/BairroService.java
@@ -1,16 +1,26 @@
 package com.example.bootcamp.services;
 
+import com.example.bootcamp.dtos.BairroDto;
 import com.example.bootcamp.models.BairroVo;
 import com.example.bootcamp.repositories.BairroRepository;
+import com.example.bootcamp.repositories.MunicipioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class BairroService {
 
     @Autowired
     private BairroRepository bairroRepository;
+    @Autowired
+    private MunicipioRepository municipioRepository;
+
+    public boolean exitsById(long id){
+        return bairroRepository.existsById(id);
+    }
 
     public List<BairroVo> findAll() {
         return bairroRepository.findAll();
@@ -20,8 +30,32 @@ public class BairroService {
         return bairroRepository.findById(id);
     }
 
-    public BairroVo save(BairroVo bairroVo) {
+    public Optional<BairroVo> findByNome(String nome){
+        return bairroRepository.findByNome(nome);
+    }
+
+    public BairroVo save(BairroDto bairroDto) {
+        BairroVo bairroVo = new BairroVo();
+        bairroVo.setNome(bairroDto.nome());
+        bairroVo.setStatus(bairroDto.status());
+        bairroVo.setMunicipio(municipioRepository.findById(bairroDto.codigoMunicipio()).get());
         return bairroRepository.save(bairroVo);
     }
 
+    public void deleteBairro(long id) {
+        bairroRepository.deleteById(id);
+    }
+
+    public BairroVo updateBairro(long id, BairroDto bairroDto) {
+        Optional<BairroVo> bairroOptional = bairroRepository.findById(id);
+
+        if (bairroOptional.isPresent()) {
+            BairroVo bairroVo = bairroOptional.get();
+            bairroVo.setNome(bairroDto.nome());
+            bairroVo.setStatus(bairroDto.status());
+            bairroVo.setMunicipio(municipioRepository.findById(bairroDto.codigoMunicipio()).get());
+            return bairroRepository.save(bairroVo);
+        }
+        return null;
+    }
 }
