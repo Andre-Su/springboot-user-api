d35227b3ce718c02480b5b81b0f347b81c422298
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2024-11-25 19:44:12 -0300

    Métodos GET, POST, PUT e DELETE GET todas as pessoas e seus endereços quando utilizado sem parâmetros GET pessoa por ID e campo LOGIN PUT por ID DELETE por ID, troca o campo "status" de 1 para 2

--- src/main/java/com/example/bootcamp/services/PessoaService.java
+++ src/main/java/com/example/bootcamp/services/PessoaService.java
@@ -7,13 +7,21 @@ import com.example.bootcamp.models.PessoaVo;
 import com.example.bootcamp.repositories.EnderecoRepository;
 import com.example.bootcamp.repositories.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;
+    @Autowired
     private EnderecoRepository enderecoRepository;
+
+    public Optional<PessoaVo> findByLogin(String login){
+        return pessoaRepository.findByLogin(login);
+    }
 
     public PessoaVo save(PessoaDto pessoaDto) {
         PessoaVo pessoa = new PessoaVo();
@@ -23,11 +31,12 @@ public class PessoaService {
         pessoa.setIdade(pessoaDto.idade());
         pessoa.setLogin(pessoaDto.login());
         pessoa.setSenha(pessoaDto.senha());
-
+        pessoaRepository.save(pessoa);
         for(EnderecoDto enderecoDto:pessoaDto.enderecos()){
             EnderecoVo endereco = new EnderecoVo();
             endereco.setNomeRua(enderecoDto.nomeRua());
             endereco.setNumero(enderecoDto.numero());
+            endereco.setComplemento(enderecoDto.complemento());
             endereco.setCep(enderecoDto.cep());
             endereco.setPessoa(pessoa);
             enderecoRepository.save(endereco);
@@ -40,4 +49,41 @@ public class PessoaService {
         return pessoaRepository.findAll();
     }
 
+
+    public Optional<PessoaVo> findById(long id) {
+        return pessoaRepository.findById(id);
+    }
+
+    public boolean existsById(long id) {
+        return pessoaRepository.existsById(id);
+    }
+
+    public PessoaVo updatePessoa(long id, PessoaDto pessoaDto) {
+        PessoaVo pessoa = pessoaRepository.findById(id).orElse(null);
+        if (pessoa!= null) {
+            pessoa.setNome(pessoaDto.nome());
+            pessoa.setStatus(pessoaDto.status());
+            pessoa.setSobrenome(pessoaDto.sobrenome());
+            pessoa.setIdade(pessoaDto.idade());
+            pessoa.setLogin(pessoaDto.login());
+            pessoa.setSenha(pessoaDto.senha());
+            for(EnderecoDto enderecoDto:pessoaDto.enderecos()){
+                EnderecoVo endereco = new EnderecoVo();
+                    endereco.setNomeRua(enderecoDto.nomeRua());
+                    endereco.setNumero(enderecoDto.numero());
+                    endereco.setComplemento(enderecoDto.complemento());
+                    endereco.setCep(enderecoDto.cep());
+                    enderecoRepository.save(endereco);
+                    pessoa.getEnderecos().add(endereco);
+            }
+            pessoaRepository.save(pessoa);
+        }
+        return pessoa;
+    }
+
+    public void deleteById(long id) {
+        PessoaVo pessoa = pessoaRepository.findById(id).get();
+        pessoa.setStatus(2);
+        pessoaRepository.save(pessoa);
+    }
 }
