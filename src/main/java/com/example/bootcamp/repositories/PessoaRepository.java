a8d5127a0689074f00a39df8bb4bb8d44b5ff801
Andre.jar <82320381+Andre-Su@users.noreply.github.com>
2024-11-25 19:28:18 -0300

    Criado método findByLogin

--- src/main/java/com/example/bootcamp/repositories/PessoaRepository.java
+++ src/main/java/com/example/bootcamp/repositories/PessoaRepository.java
@@ -3,5 +3,8 @@ package com.example.bootcamp.repositories;
 import com.example.bootcamp.models.PessoaVo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PessoaRepository extends JpaRepository<PessoaVo, Long> {
+    public Optional<PessoaVo> findByLogin(String login);
 }
